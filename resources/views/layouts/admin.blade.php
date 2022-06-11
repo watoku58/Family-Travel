a3eb995076764e5aeab7fe0b94a26c7a30745552
k-takahashi <watoku58@gmail.com>
2022-06-11 22:56:16 +0000

    cssのリセット

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -31,60 +31,54 @@
         <div id="app">
             <header>
                 {{-- 画面上部に表示するナビゲーションバーです。 --}}
-                <nav class="navbar-laravel">
-                    <div class="HeaderBox">
-                        <div class="logo">
-                            <a class="logoitem" href="{{ url('/home') }}">
-                                <img src="{{asset('storage/image/logo.png')}}" alt="写真" width="71" height="40">
-                                {{ config('app.name', 'Family') }}
-                            </a>
-                        </div>
-                        <div class="login button">
-                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-                            <div class="login-action" id="login-action">
-                                <ul class="navbar-nav ml-auto">
-                                <!-- Authentication Links -->
-                                {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
-                                @guest
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('login') }}">{{ __('messages.Login') }}</a>
-                                    </li>
-                                    @if (Route::has('register'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('register') }}">{{ __('messages.Register') }}</a>
-                                    </li>
-                                    @endif
-                                {{-- ログインしていたらユーザー名とログアウトボタンを表示 --}}
-                                @else
-                                    <li class="nav-item dropdown">
-                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                            {{ Auth::user()->name }} <span class="caret"></span>
+                <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
+                    <div class="container">
+                        <a class="navbar-brand" href="{{ url('/home') }}">
+                            <img src="{{asset('storage/image/logo.png')}}" alt="写真" width="71" height="40">
+                            {{ config('app.name', 'Family') }}
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+    
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <!-- Left Side Of Navbar -->
+                            <ul class="navbar-nav mr-auto">
+                            </ul>
+                            <!-- Right Side Of Navbar -->
+                            <ul class="navbar-nav ml-auto">
+
+                            <!-- Authentication Links -->
+                            {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
+                            @guest
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('messages.Login') }}</a>
+                                </li>
+                                @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('messages.Register') }}</a>
+                                </li>
+                                @endif
+                            {{-- ログインしていたらユーザー名とログアウトボタンを表示 --}}
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }} <span class="caret"></span>
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                         document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
                                         </a>
-                                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                               onclick="event.preventDefault();
-                                                             document.getElementById('logout-form').submit();">
-                                                {{ __('Logout') }}
-                                            </a>
-                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                                @csrf
-                                            </form>
-                                        </div>
-                                    </li>
-                                @endguest
-                                </ul>
-                            </div>
-                        </div>    
-                    </div>
-                    <div class="navigation">
-                        <ul class="navigation">
-                            <li><a href="{{ url('/') }}">トップ</a></li>
-                            <li><a href="{{ url('/user/topic/create') }}">新規投稿</a></li>
-                            <li><a href="{{ url('/user/topic') }}">投稿履歴</a></li>
-                            <li><a href="{{ url('/user/profile/browse') }}">利用者情報</a></li>
-                        </ul>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+                            </ul>
+                        </div>
                     </div>
                 </nav>
             </header>
